bf623cb8b6c97d27dc8c127c95191afdbb57ddaf
Moorthi Dharmaraj <moorthitechno@gmail.com>
2024-05-24 20:17:17 +0530

    existing files uploading

---
+++ src/App.jsx
@@ -0,0 +1,22 @@
+import { useState } from 'react'
+
+import './App.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div className='bg-blue-600 text-6xl text-yellow-300 text-center font-extrabold'>
+      Hello World!
+    </div>
+       <header>
+        
+       </header>
+       <main></main>
+       <footer></footer>
+    </>
+  )
+}
+
+export default App
